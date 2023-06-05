cedeaf2ef9fc19ec15f87d6f4d17e7ee577a466f
IsoDevMate <oumaoduor5827@gmail.com>
2023-06-05 10:35:38 +0300

    used includes method

---
+++ includes.js
@@ -0,0 +1,6 @@
+//If we want to check if item exists in the array, and don’t need the index, then arr.includes is preferred.
+
+let arr = [1, 0, false];
+
+console.log(arr.includes(false)) //true
+console.log(arr.includes(NaN))
